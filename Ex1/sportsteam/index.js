3c2f439ec165a46fc04d1a48034f26cb97878db9
mardel011 <59632640+mardel011@users.noreply.github.com>
2020-03-27 23:35:31 -0400

    Adding files

---
+++ Ex1/sportsteam/index.js
@@ -0,0 +1,8 @@
+//export named function
+exports.Cheer = function () {
+    console.log('Go! ' + this.teamname + ' Go!');
+}
+
+exports.Boo = function () {
+    console.log('Boo! ' + this.teamname + ' Boo!');
+}
